424099e2ee9cbdaf2c19e2c9221eee62ae37bba3
ZhongYunZhen <e037l862712@gmail.com>
2022-05-02 00:52:55 +0800

    Feat : test

---
+++ _next/static/qIXzLaUu5CnhTZLV0fe7u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-84673b589a56170e.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
