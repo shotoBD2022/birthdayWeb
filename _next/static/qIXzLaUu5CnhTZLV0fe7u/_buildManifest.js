a37f2137a32d85d9ec56dee216600a50b1de3dc0
ZhongYunZhen <e037l862712@gmail.com>
2022-05-29 00:41:09 +0800

    delete useless file

--- _next/static/qIXzLaUu5CnhTZLV0fe7u/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-84673b589a56170e.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
