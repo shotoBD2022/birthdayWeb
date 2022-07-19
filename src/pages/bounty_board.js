2ce1132cde332c37ec45200ca669e7bfc8425574
dida <shoto.hbd@gmail.com>
2022-07-20 00:38:22 +0800

    [Feat]:edit bounty_board layout

--- src/pages/bounty_board.js
+++ src/pages/bounty_board.js
@@ -1,21 +1,39 @@
 import { BorderLayout } from "@constant/layout";
 import { imgAuthorList } from "@lib/type";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [imgProps, setImgProps] = useState([]);
+  const [showSingle, setShowSingle] = useState(-1)
+
+  useEffect(() => {
+    const arr = imgAuthorList.map((author, i) => ({
+      url: `/img/bounty_board/pic${i + 1}.png`,
+      author,
+      scale: Math.random() * 0.1 + 1,
+      rotate: Math.random() * 10 * (i % 2 * 2 - 1)
+    }))
+    setImgProps(arr)
+  }, [])
+
   return <BorderLayout>
-    <div className="content" style={{ alignItems: "center", justifyContent: "space-evenly",paddingTop:"2em" }}>
-      {imgAuthorList.map((author, i) => <ImgCard key={i} url={`/img/bounty_board/pic${i + 1}.png`} author={author} />)}
+    <div className="content" style={{ alignItems: "center", justifyContent: "space-evenly", paddingTop: "2em" }}>
+      {imgProps.map((props, i) => <ImgCard key={i} {...props} onClick={() => setShowSingle(i)} />)}
     </div>
+    {showSingle > -1 &&
+      <div className="showCardContainer">
+        <div style={{ position: "absolute", inset: 0 }} onClick={() => setShowSingle(-1)} />
+        <ImgCard show {...imgProps[showSingle]} />
+      </div>
+    }
   </BorderLayout >
 }
 
-const ImgCard = ({ url, author }) => {
-  const transform = useMemo(() => ({ scale: Math.random() * 0.1 + 1, rotate: Math.random() * 10 - 5 }), [])
-  // const transform = useMemo(() => ({ scale: 1, rotate: 0}), [])
+const ImgCard = ({ url, author, show, scale, rotate, ...props }) => {
   return (
-    <div className=" card" style={{ transform: `rotate(${transform.rotate}deg) scale(${transform.scale})`,maxWidth:"80%" }}>
-      <img src={url} style={{ maxHeight: "20vh", maxWidth: "100%" }} />
+    <div className={"img card" + (show ? " show" : "")}
+     style={{ transform: `rotate(${rotate}deg) scale(${scale})` }} {...props} >
+      <img src={url} />
       {author && <div style={{ textAlign: "right", marginTop: ".5em" }}>by.{author}</div>}
     </div>
   )
